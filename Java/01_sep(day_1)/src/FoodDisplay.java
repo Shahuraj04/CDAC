2524f7ec76a934472f3c0dbf984f54c5f4f5fef4
Shahuraj04 <shahurajbhoite04@gmail.com>
2025-09-02 22:42:19 +0530

    02_sep_java

--- Java/01_sep(day_1)/src/FoodDisplay.java
+++ Java/01_sep(day_1)/src/FoodDisplay.java
@@ -1,83 +1,87 @@
-
 import java.util.Scanner;
 
-/*
- * 4.1 Write Java program - 
-Display food menu to user. User will select items from menu along with the quantity. (eg 1. Dosa 2. Samosa 3. Idli ... 0 . Generate Bill ) Assign fixed prices to food items(hard code the prices)
-When user enters 'Generate Bill' option(0) , display total bill & exit.
- */
 class FoodDisplay {
 
     public static void main(String[] args) {
         Scanner myscan = new Scanner(System.in);
         boolean exit = false;
         double total = 0.0;
-        int qty_sum = 0;
-        int qty = 0;
-        int qty1 = 0;
-        int qty2 = 0;
-        int qty3 = 0;
-        int qty4 = 0;
-        System.out.println("\n*********WELCOME TO CDAC CAFETERIA*********\n");
+
+        // Quantities of each item
+        int dosaQty = 0, samosaQty = 0, idliQty = 0, poheQty = 0;
+
+        System.out.println("\n=======================================");
+        System.out.println("        WELCOME TO CDAC CAFETERIA  ");
+        System.out.println("=======================================\n");
+
         while (!exit) {
+            System.out.println("============== MENU ==============");
+            System.out.printf("%-5s %-15s %s\n", "No.", "Item", "Price (Rs.)");
+            System.out.println("---------------------------------");
+            System.out.printf("%-5s %-15s %s\n", "1.", "Dosa", "50");
+            System.out.printf("%-5s %-15s %s\n", "2.", "Samosa", "25");
+            System.out.printf("%-5s %-15s %s\n", "3.", "Idli", "45");
+            System.out.printf("%-5s %-15s %s\n", "4.", "Pohe", "20");
+            System.out.println("---------------------------------");
+            System.out.println("0. Generate Bill");
+            System.out.println("=================================\n");
 
-            System.out.println("\t1. Dosa          Rs.50");
-            System.out.println("\t2. Samosa        Rs.25");
-            System.out.println("\t3. Idli          Rs.45");
-            System.out.println("\t4. Pohe          Rs.20");
-            System.out.println("\t0. GENERATE BILL");
-            System.out.print("\t\tEnter Choice: ");
-            switch (myscan.nextInt()) {
+            System.out.print("Enter Choice: ");
+            int choice = myscan.nextInt();
+
+            switch (choice) {
                 case 1:
-                    System.out.print("\t\tEnter Quantity: ");
-                    qty = myscan.nextInt();
-                    qty1 = qty;
-
-                    qty_sum = 50 * qty;
-                    total += qty_sum;
-                    qty1 += qty1;
+                    System.out.print("Enter Quantity of Dosa: ");
+                    int qty1 = myscan.nextInt();
+                    dosaQty += qty1;
+                    total += 50 * qty1;
                     break;
+
                 case 2:
-                    System.out.println("\t\tEnter Quantity: ");
-                    qty = myscan.nextInt();
-                    qty2 = qty;
-                    qty_sum = 25 * qty;
-                    total += qty_sum;
-                    qty2 += qty2;
+                    System.out.print("Enter Quantity of Samosa: ");
+                    int qty2 = myscan.nextInt();
+                    samosaQty += qty2;
+                    total += 25 * qty2;
                     break;
 
                 case 3:
-                    System.out.println("\t\tEnter Quantity: ");
-                    qty = myscan.nextInt();
-                    qty3 = qty;
-                    qty_sum = 45 * qty;
-                    total += qty_sum;
-                    qty3 += qty3;
+                    System.out.print("Enter Quantity of Idli: ");
+                    int qty3 = myscan.nextInt();
+                    idliQty += qty3;
+                    total += 45 * qty3;
                     break;
 
                 case 4:
-                    System.out.print("\t\tEnter Quantity: ");
-                    qty = myscan.nextInt();
-                    qty4 = qty;
-                    qty_sum = 45 * qty;
-                    total += qty_sum;
-                    qty4 += qty4;
+                    System.out.print("Enter Quantity of Pohe: ");
+                    int qty4 = myscan.nextInt();
+                    poheQty += qty4;
+                    total += 20 * qty4;
                     break;
 
                 case 0:
-                    System.out.println("\t\t\t#########################");
-                    System.out.println("\t\t\t#Item                Qty#");
-                    System.out.println("\t\t\t#########################");
-                    System.out.println("\t\t\tDosa               " + qty1);
-                    System.out.println("\t\t\tSamosa             " + qty2);
-                    System.out.println("\t\t\tIdli               " + qty3);
-                    System.out.println("\t\t\tPohe               " + qty4);
-                    System.out.println("\n\t\t\t***************Total: " + total + " ***************\n");
+                    System.out.println("\n============= BILL =============");
+                    System.out.printf("%-15s %-10s %-10s %s\n", "Item", "Qty", "Price", "Subtotal");
+                    System.out.println("-----------------------------------------------");
+
+                    if(dosaQty > 0)   System.out.printf("%-15s %-10d %-10d %-10.2f\n", "Dosa", dosaQty, 50, (dosaQty * 50.0));
+                    if(samosaQty > 0) System.out.printf("%-15s %-10d %-10d %-10.2f\n", "Samosa", samosaQty, 25, (samosaQty * 25.0));
+                    if(idliQty > 0)   System.out.printf("%-15s %-10d %-10d %-10.2f\n", "Idli", idliQty, 45, (idliQty * 45.0));
+                    if(poheQty > 0)   System.out.printf("%-15s %-10d %-10d %-10.2f\n", "Pohe", poheQty, 20, (poheQty * 20.0));
+
+                    System.out.println("-----------------------------------------------");
+                    System.out.printf("%-37s Rs. %.2f\n", "TOTAL:", total);
+                    System.out.println("===============================================\n");
+                    System.out.println("   Thank you! Visit Again ");
+                    System.out.println("===============================================\n");
+
                     exit = true;
+                    break;
 
+                default:
+                    System.out.println("❌ Invalid choice! Please try again.");
             }
-
         }
+
         myscan.close();
     }
 }
---
+++ Java/02_sep(day_2)/src/Point2D.java
@@ -0,0 +1,23 @@
+class Point2D {
+  private int x, y;
+
+   public Point2D(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  
+  public String show() {
+    return "(" + x + ", " + y + ")";
+  }
+
+    public boolean isEqual(Point2D obj) {
+    return this.x == obj.x && this.y == obj.y;
+  }
+
+   public double calculateDistance(Point2D obj) {
+    int xDistance = this.x - obj.x;
+    int yDistance= this.y - obj.y;
+    return Math.sqrt(xDistance*xDistance + yDistance*yDistance);
+  }
+}
