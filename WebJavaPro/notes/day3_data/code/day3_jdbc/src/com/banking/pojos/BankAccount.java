d97b8858c0377f8ac8ade7a68e262731c9127720
Shahuraj04 <shahurajbhoite04@gmail.com>
2025-10-19 13:22:04 +0530

    deleted

---
+++ WebJavaPro/notes/day3_data/code/day3_jdbc/src/com/banking/pojos/BankAccount.java
@@ -0,0 +1,55 @@
+package com.banking.pojos;
+
+//id | name | type | bal
+public class BankAccount {
+	private int acctId;
+	private String customerName;
+	private String acType;
+	private double balance;
+
+	// def ctor
+	public BankAccount() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// getters & setters
+	public int getAcctId() {
+		return acctId;
+	}
+
+	public void setAcctId(int acctId) {
+		this.acctId = acctId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getAcType() {
+		return acType;
+	}
+
+	public void setAcType(String acType) {
+		this.acType = acType;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "BankAccount [acctId=" + acctId + ", customerName=" + customerName + ", acType=" + acType + ", balance="
+				+ balance + "]";
+	}
+
+}
