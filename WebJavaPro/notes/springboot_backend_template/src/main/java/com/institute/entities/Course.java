d05b7de6ee6edd92aa892b1f5d36ac2d8e39ca5f
Shahuraj04 <shahurajbhoite04@gmail.com>
2025-11-07 21:16:34 +0530

    Student MS

--- WebJavaPro/notes/springboot_backend_template/src/main/java/com/institute/entities/Course.java
+++ WebJavaPro/notes/springboot_backend_template/src/main/java/com/institute/entities/Course.java
@@ -20,29 +20,31 @@ import lombok.NoArgsConstructor;
 
 @Table(name = "courses")
 public class Course {
-	public Course(String name2, Category category2, LocalDate startDate2, LocalDate endDate2, double fees2,
-			int requiredMarks2) {
-		this.name=name2;
-		this.category=category2;
-		this.startDate=startDate2;
-		this.endDate=endDate2;
-		this.fees=fees2;
-		this.requiredMarks=requiredMarks2;
-	}
+	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "course_id")
 	private Long courseId;
 	@Column(name = "course_name", unique = true)
 	private String name;
-	/*Course has the following attributes: 
-1.	ID (unique identifier for each Course, auto generated, starts from 1)
-2.	Name (unique , Cannot be Blank, Example : Java SE,Web Programming in Java ,Spring Boot , Spring Cloud, Oracle DB , MySql DB  )
-3.	Category (Enum, Example: JAVA,DBT)
-4.	Start Date (Cannot be Blank)
-5.	End Date (Cannot be Blank)
-6.	Fees (Cannot be Blank)
-7.	Marks to Pass (Cannot be Blank)*/
+	/*
+	 * Course has the following attributes: 1. ID (unique identifier for each
+	 * Course, auto generated, starts from 1) 2. Name (unique , Cannot be Blank,
+	 * Example : Java SE,Web Programming in Java ,Spring Boot , Spring Cloud, Oracle
+	 * DB , MySql DB ) 3. Category (Enum, Example: JAVA,DBT) 4. Start Date (Cannot
+	 * be Blank) 5. End Date (Cannot be Blank) 6. Fees (Cannot be Blank) 7. Marks to
+	 * Pass (Cannot be Blank)
+	 */
+	public Course(String name2, Category category2, LocalDate startDate2, LocalDate endDate2, double fees2,
+			int requiredMarks2) {
+		this.name = name2;
+		this.category = category2;
+		this.startDate = startDate2;
+		this.endDate = endDate2;
+		this.fees = fees2;
+		this.requiredMarks = requiredMarks2;
+	}
 	@Enumerated(EnumType.STRING)
 	private Category category;
 	@Column(name = "start_date")
@@ -53,7 +55,5 @@ public class Course {
 	private double fees;
 	@Column(name = "marks_required")
 	private int requiredMarks;
-
-	
 
 }
