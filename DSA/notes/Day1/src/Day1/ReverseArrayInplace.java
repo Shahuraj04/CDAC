f5b45b72e8aaa20d6e73bf2d0ef6fbf244af8200
Shahuraj04 <shahurajbhoite04@gmail.com>
2025-11-29 14:04:10 +0530

    DSA

---
+++ DSA/notes/Day1/src/Day1/ReverseArrayInplace.java
@@ -0,0 +1,31 @@
+package Day1;
+
+import java.util.Arrays;
+
+public class ReverseArrayInplace {
+
+	public static void main(String[] args) {
+
+		 int arr[] = {1,2,3,4,5};
+		 reverseArray(arr);
+
+	}
+
+	private static void reverseArray(int[] arr) {
+		System.out.println(Arrays.toString(arr));
+		
+		int left = 0;
+		int right=arr.length-1;
+		while(left<right) {
+				int temp  = arr[left];
+				arr[left]=arr[right];
+				arr[right] = temp;
+				
+				left++;
+				right--;
+		}
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+}
