4f301a399013e22eeb4c7a5dc5f01654e23be3f4
Shahuraj04 <shahurajbhoite04@gmail.com>
2025-12-03 16:31:50 +0530

    wpt > dotnet

--- WPT/Train Management System/train_client/pages/TrainPage.jsx
+++ WPT/Train Management System/train_client/pages/TrainPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function TrainPage() {
+
+  const navigate = useNavigate();
+  
+
   return (
-    <div></div>
+    <div>
+      <button>Show Trains</button>
+
+    </div>
   )
 }
