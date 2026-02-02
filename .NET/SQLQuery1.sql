d30bb6debc8a25e74995a00fb65694e22e27c6bc
Shahuraj04 <shahurajbhoite04@gmail.com>
2026-02-03 01:33:44 +0530

    project

---
+++ .NET/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Student
+
+(
+	[No] INT NOT NULL identity PRIMARY KEY , 
+    [Name] VARCHAR(50) NULL, 
+    [Age] INT NULL, 
+    [Email] VARCHAR(50) NULL, 
+    [Addresss] VARCHAR(50) NULL,
+	[IsEMailValidated] bit NULL
+)
